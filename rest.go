8461f015a519d79681d79de985279e55a287db67
Sandy Urazayev <ctu@ku.edu>
2021-04-11 12:51:40 -0500

    Better bad auth error return strings
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- rest.go
+++ rest.go
@@ -74,31 +74,31 @@ func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokens := strings.Split(r.Header.Get("Authorization"), "Basic ")
 		if len(tokens) != 2 {
-			httpJSON(w, nil, http.StatusBadRequest, errors.New("no auth provided"))
+			httpJSON(w, nil, http.StatusBadRequest, errors.New("no basic auth provided"))
 			return
 		}
 		decoded, err := base64.StdEncoding.DecodeString(tokens[1])
 		if err != nil {
-			httpJSON(w, nil, http.StatusBadRequest, errors.New("failed auth decoding"))
+			httpJSON(w, nil, http.StatusBadRequest, errors.New("base64 decoding failed"))
 			return
 		}
 		creds := strings.Split(string(decoded), ":")
 		if len(creds) != 2 {
-			httpJSON(w, nil, http.StatusBadRequest, errors.New("malformed auth"))
+			httpJSON(w, nil, http.StatusBadRequest, errors.New("basic auth is malformed"))
 			return
 		}
 		user, pass := creds[0], creds[1]
 		if user == "" {
-			httpJSON(w, nil, http.StatusBadRequest, errors.New("no user provided"))
+			httpJSON(w, nil, http.StatusBadRequest, errors.New("bad user credentials"))
 			return
 		}
 		foundUser, err := getUser(user)
 		if err != nil || foundUser.Name == "" {
-			httpJSON(w, nil, http.StatusForbidden, errors.New("no user found"))
+			httpJSON(w, nil, http.StatusForbidden, errors.New("requested user not found"))
 			return
 		}
 		if foundUser.Password != shaEncode(pass) {
-			httpJSON(w, nil, http.StatusForbidden, errors.New("bad login"))
+			httpJSON(w, nil, http.StatusForbidden, errors.New("wrong username or password"))
 			return
 		}
 		next.ServeHTTP(w, r)
